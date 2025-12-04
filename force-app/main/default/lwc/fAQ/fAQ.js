b44a3c5c7b9c3c0f1820952a56dc05227342756c
EnzoRazoppiTAAK <enzo.razoppi@taak.com.br>
2025-12-04 11:21:47 -0300

    Paginação finalizada

--- force-app/main/default/lwc/fAQ/fAQ.js
+++ force-app/main/default/lwc/fAQ/fAQ.js
@@ -4,6 +4,7 @@ import searchForAllFAQs from '@salesforce/apex/ApexClassToHelpLWC.searchForAllFA
 export default class FAQ extends LightningElement {
 
     fAQslist = [];
+    fAQsFilteredlist = [];
     allFAQs = [];
 
     firstQuestion;
@@ -11,12 +12,29 @@ export default class FAQ extends LightningElement {
     secondQuestion;
     secondAnswer;
 
+    pageNumbersInitial;
+    pageNumbers;
+
     typedValue = '';
 
     @wire(searchForAllFAQs) FAQs({ data, error }) {
+
         if (data) {
-            this.fAQslist = data;
-            this.allFAQs = data;
+            this.fAQslist = [...data];
+
+            if(data.length > 10){
+                this.fAQslist = this.fAQslist.splice(0, 10);
+
+                if(data.length%10 === 0){
+                    this.pageNumbersInitial = data.length/10;
+                }else{
+                    this.pageNumbersInitial = Math.floor(data.length/10) + 1;
+                }
+            }
+
+            this.calculateArrayPageNumbersInitial();
+            this.allFAQs = [...data];
+
         } else if (error) {
             this.fAQslist = undefined;
         }
@@ -38,9 +56,66 @@ export default class FAQ extends LightningElement {
     filterFAQs() {
         
         if(this.typedValue === ''){
-            this.fAQslist = this.allFAQs;
+            this.suportArray = [... this.allFAQs];
+            this.fAQslist = this.suportArray.splice(0, 10);
+            this.calculateArrayPageNumbersInitial();
         }else{
-            this.fAQslist = this.allFAQs.filter(faq => faq.Question__c.toLowerCase().includes(this.typedValue));
+            this.suportArray = [... this.allFAQs];
+            this.fAQsFilteredlist = this.allFAQs.filter(faq => faq.Question__c.toLowerCase().includes(this.typedValue));
+            this.fAQslist = this.fAQsFilteredlist.slice(0, 10);
+            this.calculateArrayPageNumbersAfterFilter(this.fAQsFilteredlist);
+        }
+    }
+
+    calculateArrayPageNumbersInitial(){
+        this.arrayPageNumbers = [];
+
+        for(let i = 1; i <= this.pageNumbersInitial; i++){
+            this.arrayPageNumbers.push({number: i, type: "original"});
+        }
+    }
+
+    calculateArrayPageNumbersAfterFilter(fAQsFilteredlist){
+        this.arrayPageNumbers = [];
+        // console.log(fAQsFilteredlist);
+        // console.log('AQUi3');
+
+
+        if(fAQsFilteredlist.length > 10){
+
+            if(fAQsFilteredlist.length%10 === 0){
+                this.pageNumbers = fAQsFilteredlist.length/10;
+            }else{
+                this.pageNumbers = Math.floor(fAQsFilteredlist.length/10) + 1;
+            }
+
+            for(let i = 1; i <= this.pageNumbers; i++){
+                this.arrayPageNumbers.push({number: i, type: "filtered"});
+            }
+
+        }else{
+            this.arrayPageNumbers.push({number: 1, type: "filtered"});
+        }
+    }
+
+    
+    changePage(event){
+
+        let desiredPage = Number(event.currentTarget.dataset.number);
+        let typePage = event.currentTarget.dataset.type;
+
+        if(typePage === "filtered"){
+
+            this.suportArray = [... this.fAQsFilteredlist];
+            console.log(this.suportArray);
+            console.log(desiredPage);
+            console.log(typePage);
+            this.fAQslist = this.suportArray.slice((desiredPage-1)*10, (desiredPage-1)*10 + 10);
+
+        }else if(typePage === "original"){
+
+            this.suportArray = [... this.allFAQs];
+            this.fAQslist = this.suportArray.splice((desiredPage-1)*10, 10);
         }
     }
 }
